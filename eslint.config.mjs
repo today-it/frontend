9c90b75e08c6d0337cb197f74a62e407dbf3f879
mj0107 <alswns1285@gmail.com>
2026-08-21 02:01:53 +0900

    chore: Storybook 설치 및 구성 #2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import storybook from 'eslint-plugin-storybook';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -27,16 +29,17 @@ const eslintConfig = defineConfig([
         },
       ],
     },
-  },
-  // Override default ignores of eslint-config-next.
+  }, // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     '.next/**',
     'out/**',
     'build/**',
+    'storybook-static/**',
     'next-env.d.ts',
   ]),
   eslintConfigPrettier,
+  ...storybook.configs['flat/recommended'],
 ]);
 
 export default eslintConfig;
