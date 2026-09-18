fb9d7b299fd22b587c57e656d187e4c8f1e71ae3
mj0107 <alswns1285@gmail.com>
2026-09-19 00:23:42 +0900

    feat: AstaSans 폰트 적용 #4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 
+import { astaSans } from '@/app/fonts';
 import Providers from '@/providers/query-provider';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -22,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="ko" className={`${astaSans.variable} h-full antialiased`}>
       <body className="flex min-h-full flex-col">
         <Providers>{children}</Providers>
       </body>
