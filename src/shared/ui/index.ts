4d573d20bda25d429a4b836afaba9481b62e7382
mj0107 <alswns1285@gmail.com>
2026-09-21 03:01:53 +0900

    feat: 공용 Chip 컴포넌트 구현 #4

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,6 +4,8 @@ export type { ButtonProps } from './button';
 export { Button, buttonVariants } from './button';
 export type { CheckboxProps } from './checkbox';
 export { Checkbox } from './checkbox';
+export type { ChipProps } from './chip';
+export { Chip } from './chip';
 export type { IconName, IconProps, IconTone } from './icon';
 export { Icon } from './icon';
 export type { IconButtonProps } from './icon-button';
