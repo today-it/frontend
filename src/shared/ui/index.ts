bba731330e33f72e547c34245f769fad8cf362d4
mj0107 <alswns1285@gmail.com>
2026-09-20 11:58:57 +0900

    feat: 공용 Button 컴포넌트 구현 #4

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,4 @@
+export type { ButtonProps } from './button';
+export { Button, buttonVariants } from './button';
 export type { IconName, IconProps, IconTone } from './icon';
 export { Icon } from './icon';
