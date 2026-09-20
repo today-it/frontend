5abba2b110522a2f9e6514169786ed9ae16be9ff
mj0107 <alswns1285@gmail.com>
2026-09-21 02:16:14 +0900

    feat: 공용 TextInput 컴포넌트 구현 #4

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,3 +4,5 @@ export type { CheckboxProps } from './checkbox';
 export { Checkbox } from './checkbox';
 export type { IconName, IconProps, IconTone } from './icon';
 export { Icon } from './icon';
+export type { TextInputProps } from './text-input';
+export { TextInput } from './text-input';
